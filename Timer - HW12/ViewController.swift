b014c8ce5ed1c8ee0a46431d367f4272f06529a2
Konstantin <kostikem165@gmail.com>
2023-01-13 00:46:10 +0300

    Initial Commit

---
+++ Timer - HW12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer - HW12
+//
+//  Created by Константин Киселёв on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
