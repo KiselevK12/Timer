f8797dac2a2bc6685da85fc69be145b4221240a4
Konstantin <kostikem165@gmail.com>
2023-01-15 01:40:00 +0300

    Added basic timer functionality

--- Timer - HW12/Sources/ViewController.swift
+++ Timer - HW12/Sources/ViewController.swift
@@ -11,13 +11,55 @@ class ViewController: UIViewController {
     
     // MARK: - UI Elements
     
-    private lazy var countingLabel: UILabel = {
+    private lazy var timeLabel: UILabel = {
         let label = UILabel()
         label.text = "25:00"
-        label.textColor = .white
+        label.textColor = .black
         label.font = UIFont.systemFont(ofSize: 60, weight: .bold)
         label.textAlignment = .center
         return label
+    }()
+    
+    var timer = Timer()
+    var isTimerStarted = false
+    var time = 25
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Пуск", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        button.backgroundColor = .systemGreen
+        button.clipsToBounds = false
+        button.layer.cornerRadius = 20
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var resetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Сброс", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        button.backgroundColor = .systemGray
+        button.clipsToBounds = false
+        button.layer.cornerRadius = 20
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        button.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        return button
     }()
     
     // MARK: - LifeCycle
@@ -36,21 +78,75 @@ class ViewController: UIViewController {
     }
     
     private func setupHierarchy() {
-        [countingLabel].forEach(view.addSubview)
+        [timeLabel,
+        startButton,
+        resetButton].forEach(view.addSubview)
     }
     
     private func setupLayout() {
         
-        countingLabel.snp.makeConstraints { make in
+        timeLabel.snp.makeConstraints { make in
             make.centerY.equalTo(view).offset(-100)
             make.centerX.equalTo(view)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(timeLabel.snp.bottom).offset(70)
+            make.height.equalTo(45)
+            make.centerX.equalTo(view).offset(80)
+            make.right.equalTo(view).offset(-50)
+        }
+        
+        resetButton.snp.makeConstraints { make in
+            make.top.equalTo(timeLabel.snp.bottom).offset(70)
+            make.height.equalTo(45)
+            make.centerX.equalTo(view).offset(-80)
+            make.left.equalTo(view).offset(50)
         }
     }
         // MARK: - Action
         
-        private func buttonPressed() {
-            
+    @objc private func startButtonTapped(_ sender: Any) {
+        resetButton.isEnabled = true
+        resetButton.alpha = 1.0
+        if !isTimerStarted {
+            startTimer()
+            isTimerStarted = true
+            startButton.setTitle("Пауза", for: .normal)
+            startButton.setTitleColor(UIColor.black, for: .normal)
+        } else {
+            timer.invalidate()
+            isTimerStarted = false
+            startButton.setTitle("Продолжить", for: .normal)
+            startButton.setTitleColor(UIColor.blue, for: .normal)
         }
+    }
+    
+    @objc private func resetButtonTapped(_ sender: Any) {
+        resetButton.isEnabled = false
+        resetButton.alpha = 0.5
+        timer.invalidate()
+        time = 25
+        isTimerStarted = false
+        timeLabel.text = "25:00"
+        startButton.setTitle("Пуск", for: .normal)
+        startButton.setTitleColor(UIColor.black, for: .normal)
+        }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.00, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        time -= 1
+        timeLabel.text = formatTime()
+    }
+    
+    private func formatTime() -> String {
+        let seconds = Int(time) % 60
+        let splitSecond = 0
+        return String(format: "%02i:%02i", seconds, splitSecond)
+    }
         
 }
 
